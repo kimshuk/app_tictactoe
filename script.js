3b63ff7fcc8ec74d69c85c56604a4a86f152fe30
Betsy Gottesman <eightamps@gmail.com>
2016-05-03 15:09:18 -0700

    commiting my most recent changes

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-var player = "X";
-var canClick = true;
-var winArray = [
-    ["0","1","2"],
-    ["3", "4", "5"],
-    ["6", "7", "8"],
-    ["0","3","6"],
-    ["1","4","7"],
-    ["2","5","8"],
-    ["0","4","8"],
-    ["2","4","6"]
-];
+var player = "X",
+    canClick = true,
+    playCount = 0,
+    winArray = [
+        ["0","1","2"],
+        ["3", "4", "5"],
+        ["6", "7", "8"],
+        ["0","3","6"],
+        ["1","4","7"],
+        ["2","5","8"],
+        ["0","4","8"],
+        ["2","4","6"]
+    ];
 
 function checkWin(playerPiece){
     var playerArray = [];
@@ -19,33 +20,39 @@ function checkWin(playerPiece){
 
         if($cell.text() === playerPiece){
             playerArray.push($cellId[$cellId.length-1]);
-            // console.log(player + "'s array: ", playerArray);
+            //we only need to save the last character of the id - the #
         }
-        // console.log(player + "'s array: ", playerArray);
     });
-    console.log("array after each: ", playerArray);
+    // console.log("array after each: ", playerArray);
     for (i = 0; i < winArray.length; i++) {
-        console.log("entering first for loop");
+        // console.log("entering first for loop");
         //enter into each item in winArray
         isWinner = true;
-        console.log(isWinner);
+        // console.log(isWinner);
         //isWinner will be set to false whenever a subArray does not meet win condition
         for (j = 0; j < winArray[i].length; j++) {
-            console.log("entering 2nd for loop");
+            // console.log("entering 2nd for loop");
             //check each item in subArrays of winArray
             if(playerArray.indexOf(winArray[i][j])===-1){
                 //if subArray[j] is not in playerArray, not a winner
                 isWinner = false;
-                console.log("isWinner inside 2nd for loop: " + isWinner);
+                // console.log("isWinner inside 2nd for loop: " + isWinner);
                 break;
+                //break out of current subArray loop and move to next one
             }
         }
         //check if isWinner is true, if so, there was a win condition, current player wins
         if(isWinner === true){
             canClick = false;
-            var $h3 = $("<h3>"+playerPiece + " wins!" + "</h3>");
-            $("#player-board").append($h3);
+            var $h3WinMessage = $("<h3>"+ playerPiece + " wins!" + "</h3>");
+            $("#player-board").append($h3WinMessage);
         }
+    }
+    if(playCount === 9 && isWinner === false){
+        //if all cells have been filled and there's no winner, it's a tie
+        canClick = false;
+        var $h3TieMessage = $("<h3>" + "game is a tie." + "</h3>");
+        $("#player-board").append($h3TieMessage);
     }
 
 }
@@ -53,18 +60,21 @@ function checkWin(playerPiece){
 $(document).ready(function(){
     $(".game-cell").on("click",function() {
         if (canClick === true) {
-
+        playCount++;
         var $this = $(this);
-        // var id = $this.attr('id');
-        // console.log(id[id.length-1]);
+
         if (player === "X") {
+
             $this.text("X");
             checkWin(player);
             player = "O";
+
         } else {
+
             $this.text("O");
             checkWin(player);
             player = "X";
+
         }
     }
 
